8a7a018ff06d4da1c652ea8b15b95bfb27e695fa
davidhckh <david.heckhoff@gmail.com>
2024-06-23 17:08:01 +0200

    fix: export components

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 export { default as Button } from "./components/Button.vue";
+export { default as Loading } from "./components/Loading.vue";
+export { default as Switch } from "./components/Switch.vue";
 
 export default {
   install(app) {
     app.component("Button", require("./components/Button.vue").default);
+    app.component("Loading", require("./components/Loading.vue").default);
+    app.component("Switch", require("./components/Switch.vue").default);
   },
 };
